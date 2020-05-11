4f0361afb797ce50ce54cf2f7472784ada0a604f
DorDob <64862484+DorDob@users.noreply.github.com>
2020-05-11 13:38:07 +0200

    Update factorial_test.py

--- tests/factorial_test.py
+++ tests/factorial_test.py
@@ -1,29 +1,15 @@
 import pytest
-from src.factorial import factorial   # wszystko z src.factorial zaimportujemy gwiazdką "*"  (tzn.obiekty, funkcje, itd.)
+from src.factorial import factorial   
 
-@pytest.mark.parametrize(   # ten test wykona się tyle razy, ile wrzucimy tu zestawów danych/ Dekorator jest nadpisywany tylko na 1 metodę i tej właśnie metody wstawiamy nasze dane, np. argument a i wynik działania f-cji ,,result"
+@pytest.mark.parametrize(  
     "n,result",
  [
-     (4,24),   # pomiędzi poszczególnymi tuplami zawierającymi dane (parametr,wynik) musimy umieścić przecinek
+     (4,24),   
      (0,1),
      (-10, False)
-
  ]
 )
 
+
 def test_factorial (n, result):
     assert factorial(n) == result
-
-# def test_add():
-#     assert 1+1 == 2           wykonując powyższy zapis, po prostu skracamy sobie zapis poniżej i piszemy mniej kodu
-#
-# def test_positive(a,result):
-#      assert factorial(a)==result
-#
-#
-# def test_zero():
-#     assert factorial(0) == 1
-#
-#
-# def test_pnegative():  #ujemna wartość
-#     assert factorial(-10) == False
